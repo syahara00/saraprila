c11c1799ac6e3c9205ef3f0b37b806f60b125347
unknown <syaharaaprilla1804@gmail.com>
2025-06-16 23:50:42 +0700

    upload tugas7c.js

---
+++ tugas7c.js
@@ -0,0 +1,8 @@
+let panjang = 20;
+let lebar = 5;
+
+let luas = panjang * lebar;
+let keliling = 2 * (panjang + lebar);
+
+console.log("Luas persegi Panjang = " + luas);
+console.log("Keliling persegi Panjang = " + keliling);
